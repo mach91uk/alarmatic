3419a330ddff84d35360d09e4c7e389150ff4733
mach91uk <github@allenby.net>
2020-02-20 19:16:15 +0000

    1306: When restoring data settings, turn off alarms, and remove ringtones (as there is not guarantee that those ringtones are on this device).

--- app/src/main/java/uk/mach91/autoalarm/util/BackupAgentUtil.java
+++ app/src/main/java/uk/mach91/autoalarm/util/BackupAgentUtil.java
@@ -13,6 +13,10 @@ import java.io.File;
 import java.io.IOException;
 
 import uk.mach91.autoalarm.R;
+import uk.mach91.autoalarm.alarms.Alarm;
+import uk.mach91.autoalarm.alarms.data.AlarmCursor;
+import uk.mach91.autoalarm.alarms.data.AlarmsTableManager;
+import uk.mach91.autoalarm.alarms.misc.AlarmController;
 
 public class BackupAgentUtil extends BackupAgent {
 
@@ -29,6 +33,29 @@ public class BackupAgentUtil extends BackupAgent {
 
     @Override
     public void onRestoreFinished () {
+        boolean someUpdates = true;
+        while (someUpdates) {
+            someUpdates = false;
+            AlarmController controller = new AlarmController(this, null);
+            // IntentService works in a background thread, so this won't hold us up.
+            AlarmCursor cursor = new AlarmsTableManager(this).queryEnabledAlarms();
+            while (cursor.moveToNext()) {
+                Alarm alarm = cursor.getItem();
+                if (alarm.isEnabled() && alarm.ringtone() != "") {
+                    someUpdates = true;
+                    Alarm newAlarm = alarm.toBuilder()
+                            .ringtone("")
+                            .build();
+                    alarm.copyMutableFieldsTo(newAlarm);
+                    newAlarm.setEnabled(false);
+                    newAlarm.ignoreUpcomingRingTime(false);
+                    controller.save(newAlarm);
+                    break;
+                }
+            }
+            cursor.close();
+        }
+
         SharedPreferences prefSetts = PreferenceManager.getDefaultSharedPreferences(this);
 
 //        boolean askedUserExp = prefSetts.getBoolean(getString(R.string.key_asked_user_experience_program), false);
@@ -41,7 +68,5 @@ public class BackupAgentUtil extends BackupAgent {
         prefsEditor2.remove(getString(R.string.key_default_alarm_tone_picker));
 
         prefsEditor2.commit();
-
     }
-
 }
